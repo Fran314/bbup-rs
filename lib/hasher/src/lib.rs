1ed76cc69ac60e29883419abaee24eadf93eda2d
Francesco Baldino <francesco.ghog@gmail.com>
2022-12-23 19:26:57 +0100

    added tests for hasher

--- lib/hasher/src/lib.rs
+++ lib/hasher/src/lib.rs
@@ -9,7 +9,7 @@ use sha2::{
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, PartialEq)]
 pub struct Hash([u8; 32]);
 
 impl Hash {
@@ -36,6 +36,11 @@ impl Hash {
 }
 impl std::fmt::Display for Hash {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+impl std::fmt::Debug for Hash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.to_hex(0))
     }
 }
@@ -60,5 +65,87 @@ pub fn hash_stream<T: std::io::Read>(mut stream: T) -> std::io::Result<Hash> {
     match std::io::copy(&mut stream, &mut hasher) {
         Ok(_) => Ok(to_hash(hasher.finalize())),
         Err(error) => Err(error),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{hash_bytes, hash_stream, Hash};
+
+    #[test]
+    fn test() {
+        to_bytes();
+        to_string();
+        hash();
+    }
+
+    fn to_bytes() {
+        for _ in 0..1000 {
+            let bytes: [u8; 32] = rand::random();
+            let other: [u8; 32] = rand::random();
+            assert_eq!(Hash(bytes).to_bytes(), bytes.to_vec());
+            if bytes != other {
+                assert_ne!(Hash(bytes).to_bytes(), other);
+            }
+        }
+    }
+
+    fn to_string() {
+        for b in 0..=15 {
+            let h = Hash([17 * b; 32]);
+            let s = format!("{b:x}");
+            for i in 1..64 {
+                assert_eq!(h.to_hex(i), s.repeat(i as usize));
+            }
+            assert_eq!(h.to_hex(0), s.repeat(64));
+            assert_eq!(h.to_hex(65), s.repeat(64));
+        }
+        for b in 0..=255 {
+            let h = Hash([b; 32]);
+            let s = format!("{b:02x}");
+            for i in 1..32 {
+                assert_eq!(h.to_hex(i * 2), s.repeat(i as usize));
+            }
+            assert_eq!(h.to_hex(0), s.repeat(32));
+            assert_eq!(h.to_hex(65), s.repeat(32));
+        }
+        for _ in 0..1000 {
+            let bytes: [u8; 32] = rand::random();
+            let mut s = String::new();
+            for byte in bytes {
+                s += format!("{byte:02x}").as_str();
+            }
+            assert_eq!(Hash(bytes).to_string(), s);
+            assert_eq!(format!("{}", Hash(bytes)), s);
+            assert_eq!(format!("{:?}", Hash(bytes)), s);
+        }
+    }
+
+    fn hash() {
+        let tests = [
+            (
+                "",
+                "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+            ),
+            (
+                "here is some random text",
+                "3ace1cf028afc2c9872ec0eb6fd25b6a083264de078e9d8459b7ea90954d52fa",
+            ),
+            (
+                "and also a different text",
+                "549f713ae4bbf70c48c4aa6a0c9b55af40ba51dd86ebcd7c77d345cdd5fe5cca",
+            ),
+            (
+                "boop beep boop bzzzz am robot executing tests",
+                "117a49851674557df82e276d46fe24453808d8bd7ada0f11142dee8ddec3ae06",
+            ),
+        ];
+        for (text, hash_val) in tests {
+            assert_eq!(hash_bytes(text).to_string(), hash_val);
+            assert_eq!(
+                hash_stream(std::io::Cursor::new(text)).unwrap().to_string(),
+                hash_val
+            );
+        }
     }
 }
